43089bd6e104a393b62276470736c66aab988633
heswap <heswap@protonmail.com>
2021-06-18 11:53:25 +0800

    add bsc deploy

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -2,6 +2,6 @@ const HeswapRouter02 = artifacts.require("HeswapRouter02");
 const FACTORY_ARTIFACT  = require('../../core/build/contracts/HeswapFactory.json');
 module.exports = async function (deployer) {
 
-  await deployer.deploy(HeswapRouter02, '0x376dD065bA81EB8746dc4D904f153b2AA62B812E', '0x7aF326B6351C8A9b8fb8CD205CBe11d4Ac5FA836');
+  await deployer.deploy(HeswapRouter02, '0x376dD065bA81EB8746dc4D904f153b2AA62B812E', '0xae13d989daC2f0dEbFf460aC112a837C89BAa7cd');
   let instanceRouter = await HeswapRouter02.deployed();
 };
